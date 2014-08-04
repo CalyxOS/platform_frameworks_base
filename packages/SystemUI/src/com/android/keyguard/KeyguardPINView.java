03d1896e86bc5336b88bbb2a0ecce9464e79fe14
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (2/2).
    Adapted to Lineage SDK and squashed with the following changes:
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 10:40:41 -0700
    
          Keyguard: Don't disable visibility when scrambling pin.
    
            Otherwise the first numbers text is flipped to invisible
            even though it shouldn't be.
    
          Change-Id: Ia2b80d594e7f11af7a60a85bdb4ea9909d0ac20b
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 16:21:27 -0700
    
          Keyguard: Fix scramblepin logic.
    
          Change-Id: I71e6d26e853fa7bfaeea4b3256902881ac7f74fc
    
      From: LuK1337 <priv.luk@gmail.com>
      Date: Sat Apr 24 18:14:46 2021 +0200
    
          Keyguard: Fix mapping mismatch between KeyEvent and NumPadKey
    
          Change-Id: Ida28a4f037c2f3fa843ac136779e026020d711db
    
    Original Change-Id: Ibe8611eb0b8a5bd9275bc96682d2cbafaa37d8f5
    Change-Id: I6716b1c76b69d51bd1457ce07de2274541fca1e8
    
    Change-Id: Iad58845071e000c2483186587dd1f6355195576b

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -34,6 +34,13 @@ import com.android.settingslib.animation.DisappearAnimationUtils;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.DevicePostureController.DevicePostureInt;
 
+import lineageos.providers.LineageSettings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Displays a PIN pad for unlocking.
  */
@@ -46,6 +53,9 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     private int mDisappearYTranslation;
     private View[][] mViews;
     @DevicePostureInt private int mLastDevicePosture = DEVICE_POSTURE_UNKNOWN;
+    private boolean mScramblePin;
+
+    private static List<Integer> sNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
 
     public KeyguardPINView(Context context) {
         this(context, null);
@@ -159,6 +169,30 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
                 new View[]{
                         null, mEcaView, null
                 }};
+
+        mScramblePin = LineageSettings.System.getInt(getContext().getContentResolver(),
+                LineageSettings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0) == 1;
+        if (mScramblePin) {
+            Collections.shuffle(sNumbers);
+            // get all children who are NumPadKey's
+            List<NumPadKey> views = new ArrayList<NumPadKey>();
+            for (int i = 0; i < mContainer.getChildCount(); i++) {
+                if (mContainer.getChildAt(i) instanceof ConstraintLayout) {
+                    ConstraintLayout nestedLayout = ((ConstraintLayout) mContainer.getChildAt(i));
+                    for (int j = 0; j < nestedLayout.getChildCount(); j++){
+                        View view = nestedLayout.getChildAt(j);
+                        if (view.getClass() == NumPadKey.class) {
+                            views.add((NumPadKey) view);
+                        }
+                    }
+                }
+            }
+            // reset the digits in the views
+            for (int i = 0; i < sNumbers.size(); i++) {
+                NumPadKey view = views.get(i);
+                view.setDigit(sNumbers.get(i));
+            }
+        }
     }
 
     @Override
@@ -208,6 +242,14 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
         mContainer.setClipToPadding(enable);
         mContainer.setClipChildren(enable);
         setClipChildren(enable);
+    }
+
+    @Override
+    protected int getNumberIndex(int number) {
+        if (mScramblePin) {
+            return (sNumbers.indexOf(number) + 1) % sNumbers.size();
+        }
+        return super.getNumberIndex(number);
     }
 
     @Override
