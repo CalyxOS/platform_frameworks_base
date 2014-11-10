573499ad6b91539e7bbedbc1ff3b90eca817b213
Roman Birg <roman@cyngn.com>
2014-11-09 19:52:34 -0800

    fixup! SystemUI: Add quick settings pull down with one finger
    Tuner service is deprecated. Use content observer instead
    
    Change-Id: Id694eb075c63eca2b3e1551d27e27d4be7327b96

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
@@ -35,9 +35,11 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.app.Fragment;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.Insets;
 import android.graphics.Rect;
 import android.graphics.Region;
+import android.os.Handler;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
@@ -101,7 +103,6 @@ import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
 import com.android.systemui.statusbar.phone.StatusBarTouchableRegionManager;
 import com.android.systemui.statusbar.policy.CastController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
-import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.LargeScreenUtils;
 import com.android.systemui.util.kotlin.JavaAdapter;
 
@@ -167,7 +168,6 @@ public class QuickSettingsController implements Dumpable {
     private final AccessibilityManager mAccessibilityManager;
     private final MetricsLogger mMetricsLogger;
     private final Resources mResources;
-    private final TunerService mTunerService;
 
     /** Whether the notifications are displayed full width (no margins on the side). */
     private boolean mIsFullWidth;
@@ -356,8 +356,7 @@ public class QuickSettingsController implements Dumpable {
             ShadeRepository shadeRepository,
             ShadeInteractor shadeInteractor,
             JavaAdapter javaAdapter,
-            CastController castController,
-            TunerService tunerService
+            CastController castController
     ) {
         mPanelViewControllerLazy = panelViewControllerLazy;
         mPanelView = panelView;
@@ -407,7 +406,19 @@ public class QuickSettingsController implements Dumpable {
 
         mLockscreenShadeTransitionController.addCallback(new LockscreenShadeTransitionCallback());
         dumpManager.registerDumpable(this);
-        mTunerService = tunerService;
+
+        mPanelView.getContext().getContentResolver().registerContentObserver(
+                LineageSettings.System.getUriFor(
+                        LineageSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN),
+                false,
+                new ContentObserver(null) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        mOneFingerQuickSettingsIntercept = LineageSettings.System.getInt(
+                                mPanelView.getContext().getContentResolver(),
+                                LineageSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
+                    }
+                });
     }
 
     @VisibleForTesting
@@ -2174,8 +2185,7 @@ public class QuickSettingsController implements Dumpable {
     }
 
     /** */
-    public final class QsFragmentListener implements FragmentHostManager.FragmentListener,
-            TunerService.Tunable {
+    public final class QsFragmentListener implements FragmentHostManager.FragmentListener {
         /** */
         @Override
         public void onFragmentViewCreated(String tag, Fragment fragment) {
@@ -2207,26 +2217,17 @@ public class QuickSettingsController implements Dumpable {
             mShadeTransitionController.setQs(mQs);
             mNotificationStackScrollLayoutController.setQsHeader((ViewGroup) mQs.getHeader());
             mQs.setScrollListener(mQsScrollListener);
-            mTunerService.addTunable(this, STATUS_BAR_QUICK_QS_PULLDOWN);
             updateExpansion();
         }
 
         /** */
         @Override
         public void onFragmentViewDestroyed(String tag, Fragment fragment) {
-            mTunerService.removeTunable(this);
             // Manual handling of fragment lifecycle is only required because this bridges
             // non-fragment and fragment code. Once we are using a fragment for the notification
             // panel, mQs will not need to be null cause it will be tied to the same lifecycle.
             if (fragment == mQs) {
                 mQs = null;
-            }
-        }
-
-        @Override
-        public void onTuningChanged(String key, String newValue) {
-            if (STATUS_BAR_QUICK_QS_PULLDOWN.equals(key)) {
-                mOneFingerQuickSettingsIntercept = TunerService.parseInteger(newValue, 1);
             }
         }
     }
