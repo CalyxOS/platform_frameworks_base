98903ac61a7321bf92d6f6a32c4a03cdc05fc43b
Sam Mortimer <sam@mortimer.me.uk>
2018-01-13 23:26:48 -0800

    SystemUI: Network Traffic [1/3]
    *) Add lineage-sdk internal NetworkTraffic to the statusbar
       using the LineageStatusBarItemHolder helper.
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu May 6 13:50:05 2021 +0200
    
        SystemUI: Force left-to-right for network traffic monitor holder
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/3445
        Change-Id: I19ea852cedfd2a69a47006977f1b74a172008058
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Apr 11 09:58:09 2021 +0200
    
        SystemUI: Hide centered_area when showing heads up
    
        Needed for network traffic monitor.
    
        Change-Id: Ib96a75a936cf4d71e23a575e012741b812ed2aa1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Apr 11 09:47:17 2021 +0200
    
        SystemUI: Move network traffic monitor under same layout as clock
    
        This effectively fixes wrongly network monitor layout on
        devices with side notch / hole punch.
    
        Change-Id: I217b886d8da75eab7636066f4822f5fd6dbc9aff
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Jan 13 23:26:48 2018 -0800
    
        SystemUI: Network Traffic [1/3]
    
        *) Add lineage-sdk internal NetworkTraffic to the statusbar
           using the LineageStatusBarItemHolder helper.
    
        Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5
    
    Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -87,6 +87,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
     private final NotificationWakeUpCoordinator mWakeUpCoordinator;
 
     private final View mClockView;
+    private final View mCenteredView;
     private final Optional<View> mOperatorNameViewOptional;
 
     @VisibleForTesting
@@ -122,12 +123,14 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
             FeatureFlags featureFlags,
             HeadsUpStatusBarView headsUpStatusBarView,
             Clock clockView,
-            @Named(OPERATOR_NAME_FRAME_VIEW) Optional<View> operatorNameViewOptional) {
+            @Named(OPERATOR_NAME_FRAME_VIEW) Optional<View> operatorNameViewOptional,
+            View centeredView) {
         super(headsUpStatusBarView);
         mNotificationIconAreaController = notificationIconAreaController;
         mNotificationRoundnessManager = notificationRoundnessManager;
         mUseRoundnessSourceTypes = featureFlags.isEnabled(Flags.USE_ROUNDNESS_SOURCETYPES);
         mHeadsUpManager = headsUpManager;
+        mCenteredView = centeredView;
 
         // We may be mid-HUN-expansion when this controller is re-created (for example, if the user
         // has started pulling down the notification shade from the HUN and then the font size
@@ -238,9 +241,15 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
                 mView.setVisibility(View.VISIBLE);
                 show(mView);
                 hide(mClockView, View.INVISIBLE);
+                if (mCenteredView.getVisibility() != View.GONE) {
+                    hide(mCenteredView, View.INVISIBLE);
+                }
                 mOperatorNameViewOptional.ifPresent(view -> hide(view, View.INVISIBLE));
             } else {
                 show(mClockView);
+                if (mCenteredView.getVisibility() != View.GONE) {
+                    show(mCenteredView);
+                }
                 mOperatorNameViewOptional.ifPresent(this::show);
                 hide(mView, View.GONE, () -> {
                     updateParentClipping(true /* shouldClip */);
