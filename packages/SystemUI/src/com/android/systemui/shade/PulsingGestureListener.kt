1df483bb73710390756e174dadb4dce22d61045f
maxwen <max.weninger@gmail.com>
2019-11-18 00:29:15 +0100

    SystemUI: use DOUBLE_TAP_TO_WAKE setting also for wake from aod
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Change-Id: Ib51fdeaaa9a1b18b79f4f311c65565352d909a72

--- packages/SystemUI/src/com/android/systemui/shade/PulsingGestureListener.kt
+++ packages/SystemUI/src/com/android/systemui/shade/PulsingGestureListener.kt
@@ -64,6 +64,7 @@ class PulsingGestureListener @Inject constructor(
 ) : GestureDetector.SimpleOnGestureListener(), Dumpable {
     private var doubleTapEnabled = false
     private var singleTapEnabled = false
+    private var doubleTapEnabledNative = false
 
     companion object {
         internal val DOUBLE_TAP_SLEEP_GESTURE =
@@ -73,8 +74,10 @@ class PulsingGestureListener @Inject constructor(
     private val quickQsOffsetHeight: Int
 
     init {
-        val tunable = Tunable { key: String?, _: String? ->
+        val tunable = Tunable { key: String?, value: String? ->
             when (key) {
+                Settings.Secure.DOUBLE_TAP_TO_WAKE ->
+                    doubleTapEnabledNative = TunerService.parseIntegerSwitch(value, false)
                 Settings.Secure.DOZE_DOUBLE_TAP_GESTURE ->
                     doubleTapEnabled = ambientDisplayConfiguration.doubleTapGestureEnabled(
                             userTracker.userId)
@@ -86,6 +89,7 @@ class PulsingGestureListener @Inject constructor(
             }
         }
         tunerService.addTunable(tunable,
+                Settings.Secure.DOUBLE_TAP_TO_WAKE,
                 Settings.Secure.DOZE_DOUBLE_TAP_GESTURE,
                 Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
                 DOUBLE_TAP_SLEEP_GESTURE)
@@ -126,7 +130,7 @@ class PulsingGestureListener @Inject constructor(
         // checks MUST be on the ACTION_UP event.
         if (e.actionMasked == MotionEvent.ACTION_UP && !falsingManager.isFalseDoubleTap) {
             if (statusBarStateController.isDozing &&
-                (doubleTapEnabled || singleTapEnabled) &&
+                (doubleTapEnabled || singleTapEnabled || doubleTapEnabledNative) &&
                 !falsingManager.isProximityNear
             ) {
                 centralSurfaces.wakeUpIfDozing(
@@ -148,6 +152,7 @@ class PulsingGestureListener @Inject constructor(
     override fun dump(pw: PrintWriter, args: Array<out String>) {
         pw.println("singleTapEnabled=$singleTapEnabled")
         pw.println("doubleTapEnabled=$doubleTapEnabled")
+        pw.println("doubleTapEnabledNative=$doubleTapEnabledNative")
         pw.println("doubleTapToSleepEnabled=$doubleTapToSleepEnabled")
         pw.println("isDocked=${dockManager.isDocked}")
         pw.println("isProxCovered=${falsingManager.isProximityNear}")
