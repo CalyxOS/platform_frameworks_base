72672c5170d34d05a61602e8b54687135d9dc04c
Oliver Scott <olivercscott@gmail.com>
2021-06-29 21:55:04 -0400

    Compare authorities without user id
    Change-Id: I2760723511be413fca8a92f8ccd1154ec7915333

--- core/java/android/provider/DocumentsProvider.java
+++ core/java/android/provider/DocumentsProvider.java
@@ -145,6 +145,8 @@ import java.util.Objects;
 public abstract class DocumentsProvider extends ContentProvider {
     private static final String TAG = "DocumentsProvider";
 
+    private static final String SEEDVAULT_PACKAGE = "com.stevesoltys.seedvault";
+
     private static final int MATCH_ROOTS = 1;
     private static final int MATCH_ROOT = 2;
     private static final int MATCH_RECENT = 3;
@@ -1116,8 +1118,19 @@ public abstract class DocumentsProvider extends ContentProvider {
         final String documentId = DocumentsContract.getDocumentId(documentUri);
 
         if (!mAuthority.equals(authority)) {
-            throw new SecurityException(
-                    "Requested authority " + authority + " doesn't match provider " + mAuthority);
+            // If the calling package is Seedvault, check authorities without user id
+            // (allows secondary users to backup to USB). Else, keep default behaviour.
+            if (getCallingPackage().equals(SEEDVAULT_PACKAGE) &&
+                mAuthority.equals("com.android.externalstorage.documents") &&
+                enforceCallingPermission(INTERACT_ACROSS_USERS)) {
+                if (!mAuthority.equals(ContentProvider.getAuthorityWithoutUserId(authority))) {
+                    throw new SecurityException(
+                            "Requested authority " + authority + " doesn't match provider " + mAuthority);
+                }
+            } else {
+                throw new SecurityException(
+                        "Requested authority " + authority + " doesn't match provider " + mAuthority);
+            }
         }
 
         if (METHOD_IS_CHILD_DOCUMENT.equals(method)) {
