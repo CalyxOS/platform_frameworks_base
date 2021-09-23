9e9e65d7322760466050fc4bd2816904376935b8
Oliver Scott <olivercscott@gmail.com>
2021-09-22 21:38:26 -0400

    Hide "Enable Google Play Services" notification
    Issue: calyxos#554
    Change-Id: Iafeee04d854a286c8e51444d07be546244efd9b4

--- services/core/java/com/android/server/notification/PreferencesHelper.java
+++ services/core/java/com/android/server/notification/PreferencesHelper.java
@@ -1219,6 +1219,9 @@ public class PreferencesHelper implements RankingConfig {
                     channel.setShowBadge(false);
                 }
                 channel.setOriginalImportance(channel.getImportance());
+                if ("com.google.android.gms.availability".equals(channel.getId())) {
+                    channel.setImportance(IMPORTANCE_NONE);
+                }
 
                 // validate parent
                 if (channel.getParentChannelId() != null) {
