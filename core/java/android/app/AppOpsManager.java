lin15-microG/android_frameworks_base

AppOps: Default GET_USAGE_STATS to MODE_IGNORED
The AppOp OP_GET_USAGE_STATS is defaulted with MODE_DEFAULT and this is
resolved to default to ALLOW, if the permission PACKAGE_USAGE_STATS is
requested. This can be switched off in a specific settings menu, hence
an opt-out is implemented in AOSP.

Letting 3rd parties analyze the behavior does not really add any value
for the device holder, hence an opt-in makes more sense. Usage stats
collection is now disabled by default for apps requesting that permission.

If the user wants to allow stats collection, he can enter the respective
menu in settings and allow the app to collect usage data.

Change-Id: I9e08822851cf660277e45f3023aa80d8918f45ae

core/java/android/app/AppOpsManager.java
@@ -1050,7 +1050,7 @@ public class AppOpsManager {
             AppOpsManager.MODE_ALLOWED,
             AppOpsManager.MODE_ALLOWED,
             AppOpsManager.MODE_ALLOWED,
-            AppOpsManager.MODE_DEFAULT, // OP_GET_USAGE_STATS
+            AppOpsManager.MODE_IGNORED, // OP_GET_USAGE_STATS
             AppOpsManager.MODE_ALLOWED,
             AppOpsManager.MODE_ALLOWED,
             AppOpsManager.MODE_IGNORED, // OP_PROJECT_MEDIA
@@ -1138,7 +1138,7 @@ public class AppOpsManager {
             AppOpsManager.MODE_ALLOWED, // OP_WAKE_LOCK
             AppOpsManager.MODE_ALLOWED, // OP_MONITOR_LOCATION
             AppOpsManager.MODE_ASK,     // OP_MONITOR_HIGH_POWER_LOCATION
-            AppOpsManager.MODE_DEFAULT, // OP_GET_USAGE_STATS
+            AppOpsManager.MODE_IGNORED, // OP_GET_USAGE_STATS
             AppOpsManager.MODE_ALLOWED, // OP_MUTE_MICROPHONE
             AppOpsManager.MODE_ALLOWED, // OP_TOAST_WINDOW
             AppOpsManager.MODE_IGNORED, // OP_PROJECT_MEDIA

CalyxOS/platform_frameworks_base

core/java/android/app/AppOpsManager.java
@@ -2282,7 +2282,7 @@ public class AppOpsManager {
             AppOpsManager.MODE_ALLOWED, // WAKE_LOCK
             AppOpsManager.MODE_ALLOWED, // MONITOR_LOCATION
             AppOpsManager.MODE_ALLOWED, // MONITOR_HIGH_POWER_LOCATION
-            AppOpsManager.MODE_DEFAULT, // GET_USAGE_STATS
+            AppOpsManager.MODE_IGNORED, // GET_USAGE_STATS
             AppOpsManager.MODE_ALLOWED, // MUTE_MICROPHONE
             AppOpsManager.MODE_ALLOWED, // TOAST_WINDOW
             AppOpsManager.MODE_IGNORED, // PROJECT_MEDIA