GrapheneOS-Archive/platform_frameworks_base-old

automatically reboot device after timeout if set

core/java/android/provider/Settings.java
@@ -14446,6 +14446,13 @@ public final class Settings {
          * @hide
          */
         public static final String BLUETOOTH_OFF_TIMEOUT = "bluetooth_off_timeout";
+
+        /**
+         * Whether to automatically reboot the device after a user defined timeout
+         *
+         * @hide
+         */
+         public static final String SETTINGS_REBOOT_AFTER_TIMEOUT = "settings_reboot_after_timeout";
     }
 
     /**

CalyxOS/platform_frameworks_base

Change-Id: Icc690d1a350813d9767ed8d732f633a3542ee6aa
(cherry picked from commit 9a7b15da783f353cb8823062552ee454d3365430)

core/java/android/provider/Settings.java
@@ -16183,6 +16183,13 @@ public final class Settings {
          * @hide
          */
         public static final String CLEARTEXT_NETWORK_POLICY = "cleartext_network_policy";
+
+        /**
+         * Whether to automatically reboot the device after a user defined timeout
+         *
+         * @hide
+         */
+        public static final String DEVICE_REBOOT_TIMEOUT = "device_reboot_timeout";
     }
 
     /**