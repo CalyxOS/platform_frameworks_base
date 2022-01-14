9b92cc92a9f8768776b2e7da86f6c25b02c7de93
Chirayu Desai <chirayudesai1@gmail.com>
2022-01-14 22:23:13 +0530

    hasRestrictedModeAccess
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Change-Id: I5c0f8b4848558bd6ea10cfac25c8490b3e5a4580

--- services/core/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -4260,26 +4260,27 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
     private boolean hasRestrictedModeAccess(int uid) {
         final long token = Binder.clearCallingIdentity();
         try {
-            NetworkCapabilities nc = mConnManager.getNetworkCapabilities(
-                    mConnManager.getActiveNetwork());
-            Binder.restoreCallingIdentity(token);
-            int policy = getUidPolicy(uid);
-            if (nc != null
-                    && ((nc.hasTransport(TRANSPORT_VPN) && ((policy & POLICY_REJECT_VPN) != 0))
-                    || (nc.hasTransport(TRANSPORT_CELLULAR) && ((policy & POLICY_REJECT_CELLULAR)
-                    != 0))
-                    || (nc.hasTransport(TRANSPORT_WIFI) && ((policy & POLICY_REJECT_WIFI) != 0)))) {
-                return false;
-            }
             // TODO: this needs to be kept in sync with
             // PermissionMonitor#hasRestrictedNetworkPermission
-            return ConnectivitySettingsManager.getUidsAllowedOnRestrictedNetworks(mContext)
+            // Check for restricted-networking-mode status
+            final boolean isUidAllowedOnRestrictedNetworks = 
+                    ConnectivitySettingsManager.getUidsAllowedOnRestrictedNetworks(mContext)
                     .contains(uid)
                     || mIPm.checkUidPermission(CONNECTIVITY_USE_RESTRICTED_NETWORKS, uid)
                     == PERMISSION_GRANTED
                     || mIPm.checkUidPermission(NETWORK_STACK, uid) == PERMISSION_GRANTED
                     || mIPm.checkUidPermission(PERMISSION_MAINLINE_NETWORK_STACK, uid)
                     == PERMISSION_GRANTED;
+            NetworkCapabilities nc = mConnManager.getNetworkCapabilities(
+                    mConnManager.getActiveNetwork());
+            Binder.restoreCallingIdentity(token);
+            int policy = getUidPolicy(uid);
+            final boolean isUidRestrictedByPolicy = (nc != null
+                    && ((nc.hasTransport(TRANSPORT_VPN) && ((policy & POLICY_REJECT_VPN) != 0))
+                    || (nc.hasTransport(TRANSPORT_CELLULAR) && ((policy & POLICY_REJECT_CELLULAR)
+                    != 0))
+                    || (nc.hasTransport(TRANSPORT_WIFI) && ((policy & POLICY_REJECT_WIFI) != 0))))
+            return isUidAllowedOnRestrictedNetworks && !isUidRestrictedByPolicy;
         } catch (RemoteException e) {
             return false;
         } finally {
