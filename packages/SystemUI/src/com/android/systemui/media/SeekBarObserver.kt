0ae87e65bffc3769290cbf10276f66b4c259c184
t-m-w <tmw-likes-calyx@pm.me>
2022-09-21 15:08:56 +0000

    [DO NOT MERGE] Disable SquigglyProgress seekbar animation
    TODO: If incorporated, it should probably be as a toggle.
    
    Stop the media control center / notification from animating the
    seekbar.
    
    Currently has only been tested in an emulator which already failed to
    actually play audio, so it would need to be verified somewhere else.
    
    Other interesting areas:
    - R.dimen.qs_media_seekbar_progress_*
      - wavelength
      - amplitude
      - phase
      - stroke_width
    
    Could possibly accomplish this with an overlay on amplitude, but then a
    toggle may not be easily possible; not sure.
    
    Change-Id: I3aa7467b49a073e33f7d52bf96931a5dd4276f0e

--- packages/SystemUI/src/com/android/systemui/media/SeekBarObserver.kt
+++ packages/SystemUI/src/com/android/systemui/media/SeekBarObserver.kt
@@ -88,7 +88,6 @@ open class SeekBarObserver(
 
         holder.seekBar.thumb.alpha = if (data.seekAvailable) 255 else 0
         holder.seekBar.isEnabled = data.seekAvailable
-        progressDrawable?.animate = data.playing && !data.scrubbing
         progressDrawable?.transitionEnabled = !data.seekAvailable
 
         if (holder.seekBar.maxHeight != seekBarEnabledMaxHeight) {
