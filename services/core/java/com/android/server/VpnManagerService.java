8e4ba2c8ee46b2ecb7829e1765317a1f2b212a49
Oliver Scott <olivercscott@gmail.com>
2023-01-05 19:44:46 -0500

    Refresh vpn lockdown allowlist on package added/removed
    Change-Id: Ie609ea275ed339ff67928a79701a123f883618fd

--- services/core/java/com/android/server/VpnManagerService.java
+++ services/core/java/com/android/server/VpnManagerService.java
@@ -900,6 +900,8 @@ public class VpnManagerService extends IVpnManager.Stub {
             }
 
             vpn.refreshPlatformVpnAppExclusionList();
+            vpn.setAlwaysOnPackage(vpn.getAlwaysOnPackage(), vpn.getLockdown(),
+                    vpn.getLockdownAllowlist());
 
             if (TextUtils.equals(vpn.getPackage(), packageName) && userId == UserHandle.USER_SYSTEM
                     && vpn.isGlobalVpn()) {
@@ -921,6 +923,8 @@ public class VpnManagerService extends IVpnManager.Stub {
 
             if (vpn != null && !isReplacing) {
                 vpn.refreshPlatformVpnAppExclusionList();
+                vpn.setAlwaysOnPackage(vpn.getAlwaysOnPackage(), vpn.getLockdown(),
+                        vpn.getLockdownAllowlist());
             }
         }
     }
