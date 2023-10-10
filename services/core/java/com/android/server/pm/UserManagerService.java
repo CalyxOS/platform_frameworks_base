OrionOS-Project/frameworks_base

Show confirm credential activity on parent when unpausing work.
This fixes an issue affecting HSUM where the credential activity
was shown on user 0 rather than on the parent of the profile.

Bug: 272704160
Test: manual, flash and test unpausing profile
Change-Id: I97f4ecea33933fe7c404330a056cb274cfc1ed0a

services/core/java/com/android/server/pm/UserManagerService.java
@@ -1495,7 +1495,8 @@ public class UserManagerService extends IUserManager.Stub {
         // intentSender
         unlockIntent.putExtra(Intent.EXTRA_INTENT, pendingIntent.getIntentSender());
         unlockIntent.setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        mContext.startActivity(unlockIntent);
+        mContext.startActivityAsUser(
+                unlockIntent, UserHandle.of(getProfileParentIdUnchecked(userId)));
     }
 
     @Override

CalyxOS/platform_frameworks_base

services/core/java/com/android/server/pm/UserManagerService.java
@@ -1334,7 +1334,8 @@ public class UserManagerService extends IUserManager.Stub {
         // intentSender
         unlockIntent.putExtra(Intent.EXTRA_INTENT, pendingIntent.getIntentSender());
         unlockIntent.setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        mContext.startActivity(unlockIntent);
+        mContext.startActivityAsUser(
+                unlockIntent, UserHandle.of(getProfileParentId(userId)));
     }
 
     @Override