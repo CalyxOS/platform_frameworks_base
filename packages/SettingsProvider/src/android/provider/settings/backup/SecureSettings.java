9ef97ebdad21f1d14e12108d6c66557467225cd5
Oliver Scott <olivercscott@gmail.com>
2024-04-17 23:47:06 -0400

    Microphone timeout feature
    Change-Id: I663663c45c5154f0142484124fcaf0831717aed1

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -259,6 +259,7 @@ public class SecureSettings {
         Settings.Secure.EVEN_DIMMER_ACTIVATED,
         Settings.Secure.EVEN_DIMMER_MIN_NITS,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
-        Settings.Secure.CAMERA_OFF_TIMEOUT
+        Settings.Secure.CAMERA_OFF_TIMEOUT,
+        Settings.Secure.MIC_OFF_TIMEOUT
     };
 }
