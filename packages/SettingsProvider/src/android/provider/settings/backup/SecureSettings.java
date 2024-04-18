990135a304c338765d9bb5ca4c96d7e67e4cec5d
Oliver Scott <olivercscott@gmail.com>
2024-04-17 23:31:13 -0400

    Camera timeout feature
    Issue: calyxos#1613
    Issue: calyxos#1597
    Change-Id: I6db737536334459146753532d0cb6937c6cd2601

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -294,6 +294,7 @@ public class SecureSettings {
         Settings.Secure.STRICT_LOCATION_REDACTION,
         Settings.Secure.ADVANCED_PROTECTION_MODE,
         Settings.Secure.ACCESSIBILITY_KEY_GESTURE_TARGETS,
+        Settings.Secure.CAMERA_OFF_TIMEOUT,
         Settings.Secure.EM_VALUE,
         Settings.Secure.FACE_APP_ENABLED,
         Settings.Secure.FACE_KEYGUARD_ENABLED,
--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -466,6 +466,21 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.TETHERING_ALLOW_VPN_UPSTREAMS, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.STRICT_LOCATION_REDACTION, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.ADVANCED_PROTECTION_MODE, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.CAMERA_OFF_TIMEOUT,
+                new DiscreteValueValidator(new String[] {
+                        "0",
+                        "15000",
+                        "30000",
+                        "60000",
+                        "120000",
+                        "300000",
+                        "600000",
+                        "1800000",
+                        "3600000",
+                        "7200000",
+                        "14400000",
+                        "28800000"
+                }));
         VALIDATORS.put(Secure.AAPM_USB_DATA_PROTECTION, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.DISABLE_ADAPTIVE_AUTH_LIMIT_LOCK, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.FACE_APP_ENABLED,  BOOLEAN_VALIDATOR);
