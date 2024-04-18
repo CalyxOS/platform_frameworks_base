4a777b594165b2665db0e3c8878d4bb79264336f
Oliver Scott <olivercscott@gmail.com>
2024-04-17 23:31:13 -0400

    Camera timeout feature
    Issue: calyxos#1613
    Issue: calyxos#1597
    Change-Id: I6db737536334459146753532d0cb6937c6cd2601

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -269,6 +269,7 @@ public class SecureSettings {
         Settings.Secure.CAMERA_EXTENSIONS_FALLBACK,
         Settings.Secure.VISUAL_QUERY_ACCESSIBILITY_DETECTION_ENABLED,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
-        Settings.Secure.STRICT_LOCATION_REDACTION
+        Settings.Secure.STRICT_LOCATION_REDACTION,
+        Settings.Secure.CAMERA_OFF_TIMEOUT
     };
 }
