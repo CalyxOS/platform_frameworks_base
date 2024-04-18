1e8f272722f0c471f05fd9e3e54e84e7d44db056
Oliver Scott <olivercscott@gmail.com>
2024-04-17 23:31:13 -0400

    Camera timeout feature
    Issue: calyxos#1613
    Issue: calyxos#1597
    Change-Id: I6db737536334459146753532d0cb6937c6cd2601

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -308,6 +308,7 @@ public class SecureSettings {
         Settings.Secure.ON_DEVICE_INTELLIGENCE_IDLE_TIMEOUT_MS,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.STRICT_LOCATION_REDACTION,
+        Settings.Secure.CAMERA_OFF_TIMEOUT,
         Settings.Secure.MANDATORY_BIOMETRICS,
         Settings.Secure.MANDATORY_BIOMETRICS_REQUIREMENTS_SATISFIED,
         Settings.Secure.ADVANCED_PROTECTION_MODE,
