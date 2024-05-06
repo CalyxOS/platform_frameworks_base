ae266082d2d3b9624a0d68e7ed8be7051707079d
Oliver Scott <olivercscott@gmail.com>
2024-05-06 15:32:27 -0400

    Implement internet access for new apps setting
    React as early as possible to PACKAGE_ADDED to avoid race conditions
    
    Issue: calyxos#2112
    Change-Id: I454dfbca879067f35cfd011122f5a8b76a1c7f6b

--- services/core/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -323,6 +323,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.IntConsumer;
 import java.util.stream.Collectors;
 
+import lineageos.providers.LineageSettings;
+
 /**
  * Service that maintains low-level network policy rules, using
  * {@link NetworkStatsService} statistics to drive those rules.
@@ -1339,8 +1341,11 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
                 // global background data policy
                 // Clear the cache for the app
                 synchronized (mUidRulesFirstLock) {
-                    if (!hasInternetPermissionUL(uid) && !isSystemApp(uid)) {
-                        Slog.i(TAG, "ACTION_PACKAGE_ADDED for uid=" + uid + ", no INTERNET");
+                    if ((!hasInternetPermissionUL(uid) && !isSystemApp(uid)) ||
+                            LineageSettings.Secure.getIntForUser(mContext.getContentResolver(),
+                                    LineageSettings.Secure.DEFAULT_RESTRICT_NETWORK_DATA, 0,
+                                    UserHandle.getUserId(uid)) == 1) {
+                        Slog.i(TAG, "ACTION_PACKAGE_ADDED for uid=" + uid + ", no internet");
                         addUidPolicy(uid, POLICY_REJECT_ALL);
                     }
                     mInternetPermissionMap.delete(uid);
