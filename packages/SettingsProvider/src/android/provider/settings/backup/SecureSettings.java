1e138c2c97911e609549a2323f25e95503188288
Tommy Webb <tommy@calyxinstitute.org>
2024-06-24 17:28:28 +0000

    Settings.Secure: Add STRICT_LOCATION_REDACTION
    Issue: calyxos#2468
    Change-Id: Ic6bb59a32fe9441ecc8488c97c80e1ecbb51d5a1

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -280,6 +280,7 @@ public class SecureSettings {
         Settings.Secure.ON_DEVICE_INTELLIGENCE_UNBIND_TIMEOUT_MS,
         Settings.Secure.ON_DEVICE_INFERENCE_UNBIND_TIMEOUT_MS,
         Settings.Secure.ON_DEVICE_INTELLIGENCE_IDLE_TIMEOUT_MS,
-        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+        Settings.Secure.STRICT_LOCATION_REDACTION
     };
 }
