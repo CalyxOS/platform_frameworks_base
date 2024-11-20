7a03ada57f26a2789de0f8f4dfd04271e782fd62
Tommy Webb <tommy@calyxinstitute.org>
2024-11-20 14:33:30 -0500

    Allow use of a separate process for app backup
    Controlled by the persist.testing.backup_agent_in_separate_process
    system property.
    
    Issue: calyxos#2816
    Change-Id: Ib1e0b6665089069fcec2df979a693e51d9605033

--- core/java/android/app/ActivityManagerInternal.java
+++ core/java/android/app/ActivityManagerInternal.java
@@ -43,6 +43,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PowerExemptionManager.ReasonCode;
 import android.os.PowerExemptionManager.TempAllowListType;
+import android.os.SystemProperties;
 import android.os.TransactionTooLargeException;
 import android.os.WorkSource;
 import android.os.instrumentation.IOffsetCallback;
@@ -1385,4 +1386,9 @@ public abstract class ActivityManagerInternal {
      * @hide
      */
     public abstract void addCreatorToken(Intent intent, String creatorPackage);
+
+    public static boolean enableBackupAgentInSeparateProcess() {
+        return SystemProperties.getBoolean("persist.testing.backup_agent_in_separate_process",
+                false);
+    }
 }
