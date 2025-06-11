929d9e43fa16f0800ccbd15338fbb3614d37e539
Michael Bestas <mkbestas@gmail.com>
2025-06-11 21:40:40 +0300

    Revert "SystemUI: Fix burn-in translations not applying to keyguard slices"
    This reverts commit e7ba78d7c410754d1b9d728914aef82d19c7c3c7.
    
    Reason for revert: Conflicts with upstream changes
    
    Change-Id: I150b84ed1a615b25dab2c215d00716a110ac12a7

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
@@ -186,7 +186,6 @@ object KeyguardRootViewBinder {
                             if (KeyguardBottomAreaRefactor.isEnabled) {
                                 childViews[statusViewId]?.alpha = alpha
                                 childViews[burnInLayerId]?.alpha = alpha
-                                childViews[sliceViewId]?.alpha = alpha
                             }
                         }
                     }
@@ -199,7 +198,6 @@ object KeyguardRootViewBinder {
                             // same as translationX
                             viewModel.translationY.collect { y ->
                                 childViews[burnInLayerId]?.translationY = y
-                                childViews[sliceViewId]?.translationY = y
                                 childViews[largeClockId]?.translationY = y
                                 childViews[aodNotificationIconContainerId]?.translationY = y
                             }
@@ -212,7 +210,6 @@ object KeyguardRootViewBinder {
                                     state.isToOrFrom(KeyguardState.AOD) -> {
                                         // Large Clock is not translated in the x direction
                                         childViews[burnInLayerId]?.translationX = px
-                                        childViews[sliceViewId]?.translationX = px
                                         childViews[aodNotificationIconContainerId]?.translationX =
                                             px
                                     }
@@ -270,7 +267,6 @@ object KeyguardRootViewBinder {
                         launch {
                             viewModel.burnInLayerVisibility.collect { visibility ->
                                 childViews[burnInLayerId]?.visibility = visibility
-                                childViews[sliceViewId]?.visibility = visibility
                             }
                         }
 
@@ -620,7 +616,6 @@ object KeyguardRootViewBinder {
 
     private val statusViewId = R.id.keyguard_status_view
     private val burnInLayerId = R.id.burn_in_layer
-    private val sliceViewId = R.id.keyguard_slice_view
     private val aodNotificationIconContainerId = R.id.aod_notification_icon_container
     private val largeClockId = customR.id.lockscreen_clock_view_large
     private val smallClockId = customR.id.lockscreen_clock_view
