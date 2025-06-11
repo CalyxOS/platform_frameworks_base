125ee65bbf0366317d2b1a30531afb9b40fc006d
Michael Bestas <mkbestas@gmail.com>
2025-06-11 19:12:24 +0300

    Revert "Support work profiles on full secondary users"
    This reverts commit ef21b025d42da41d077911fcabaca0e52de38d56.
    
    Reason for revert: Upstream added aconfig flag for this
    
    Change-Id: I9e12b9bf108ef2786157db1e5485407e3276f181

--- core/java/android/content/pm/UserInfo.java
+++ core/java/android/content/pm/UserInfo.java
@@ -486,7 +486,7 @@ public class UserInfo implements Parcelable {
         if (isProfile() || isGuest() || isRestricted()) {
             return false;
         }
-        return isMain() || isFull();
+        return isMain();
     }
 
     // TODO(b/142482943): Get rid of this (after removing it from all tests) if feasible.
