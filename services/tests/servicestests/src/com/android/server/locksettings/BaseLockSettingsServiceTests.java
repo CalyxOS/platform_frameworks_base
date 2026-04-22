5adf32bd7b8800258280aa4b2328f88a8640eafe
Michael Bestas <mkbestas@gmail.com>
2026-04-22 09:02:59 +0000

    Revert "Remove frp_enforcement flag."
    This reverts commit 165cc54f1ceab81a389c133f29d9abb072a803ca.
    
    Reason for revert: Temporarily revert the old FRP code so that affected
    users can unlock their devices.
    
    Change-Id: Id77e073af7f687062995de01a1326f012b47a8c3

--- services/tests/servicestests/src/com/android/server/locksettings/BaseLockSettingsServiceTests.java
+++ services/tests/servicestests/src/com/android/server/locksettings/BaseLockSettingsServiceTests.java
@@ -319,7 +319,9 @@ public abstract class BaseLockSettingsServiceTests {
     }
 
     protected void setSecureFrpMode(boolean secure) {
-        mStorage.setTestFactoryResetProtectionState(secure);
+        if (android.security.Flags.frpEnforcement()) {
+            mStorage.setTestFactoryResetProtectionState(secure);
+        }
         Settings.Secure.putIntForUser(mContext.getContentResolver(),
                 Settings.Secure.SECURE_FRP_MODE, secure ? 1 : 0, UserHandle.USER_SYSTEM);
     }
