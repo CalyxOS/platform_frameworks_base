43c1362a540ff4e0a554bbe1aea2130feb383fab
Michael Bestas <mkbestas@gmail.com>
2026-04-22 09:02:59 +0000

    Revert "Remove frp_enforcement flag."
    This reverts commit 165cc54f1ceab81a389c133f29d9abb072a803ca.
    
    Reason for revert: Temporarily revert the old FRP code so that affected
    users can unlock their devices.
    
    Change-Id: Id77e073af7f687062995de01a1326f012b47a8c3

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -75,6 +75,7 @@ import android.app.admin.PasswordMetrics;
 import android.app.trust.IStrongAuthTracker;
 import android.app.trust.TrustManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -1308,7 +1309,15 @@ public class LockSettingsService extends ILockSettings.Stub {
             return;
         }
 
-        if (mStorage.isFactoryResetProtectionActive()) {
+        final ContentResolver cr = mContext.getContentResolver();
+        final boolean inSetupWizard = Settings.Secure.getIntForUser(cr,
+                Settings.Secure.USER_SETUP_COMPLETE, 0, mainUserId) == 0;
+        final boolean isFrpActive = android.security.Flags.frpEnforcement()
+                ? mStorage.isFactoryResetProtectionActive()
+                : (Settings.Global.getInt(cr, Settings.Global.SECURE_FRP_MODE, 0) == 1)
+                        && inSetupWizard;
+
+        if (isFrpActive) {
             throw new SecurityException("Cannot change credential while factory reset protection"
                     + " is active");
         }
@@ -2463,7 +2472,8 @@ public class LockSettingsService extends ILockSettings.Stub {
             if (isSpecialUserId(userId)) {
                 response = mSpManager.verifySpecialUserCredential(userId, getGateKeeperService(),
                         credential, progressCallback);
-                if (response.isMatched() && userId == USER_FRP) {
+                if (android.security.Flags.frpEnforcement() && response.isMatched()
+                        && userId == USER_FRP) {
                     mStorage.deactivateFactoryResetProtectionWithoutSecret();
                 }
                 return reportResultToSoftwareRateLimiter(response, lskfId, credential);
@@ -3255,6 +3265,10 @@ public class LockSettingsService extends ILockSettings.Stub {
     }
 
     private void sendMainUserCredentialChangedNotificationIfNeeded(int userId) {
+        if (!android.security.Flags.frpEnforcement()) {
+            return;
+        }
+
         if (userId != mInjector.getUserManagerInternal().getMainUserId()) {
             return;
         }
--- services/tests/servicestests/src/com/android/server/locksettings/LockSettingsServiceTests.java
+++ services/tests/servicestests/src/com/android/server/locksettings/LockSettingsServiceTests.java
@@ -589,6 +589,15 @@ public class LockSettingsServiceTests extends BaseLockSettingsServiceTests {
     }
 
     @Test
+    @DisableFlags(android.security.Flags.FLAG_FRP_ENFORCEMENT)
+    public void testSetCredentialPossibleInSecureFrpModeAfterSuw_FlagOff() throws RemoteException {
+        setUserSetupComplete(true);
+        setSecureFrpMode(true);
+        setCredential(PRIMARY_USER_ID, newPassword("1234"));
+    }
+
+    @Test
+    @EnableFlags(android.security.Flags.FLAG_FRP_ENFORCEMENT)
     public void testSetCredentialNotPossibleInSecureFrpModeAfterSuw_FlagOn()
             throws RemoteException {
         setUserSetupComplete(true);
@@ -1067,13 +1076,20 @@ public class LockSettingsServiceTests extends BaseLockSettingsServiceTests {
     }
 
     private void checkRecordedFrpNotificationIntent() {
-        Intent savedNotificationIntent = mService.getSavedFrpNotificationIntent();
-        assertNotNull(savedNotificationIntent);
-        UserHandle userHandle = mService.getSavedFrpNotificationUserHandle();
-        assertEquals(userHandle, UserHandle.of(mInjector.getUserManagerInternal().getMainUserId()));
+        if (android.security.Flags.frpEnforcement()) {
+            Intent savedNotificationIntent = mService.getSavedFrpNotificationIntent();
+            assertNotNull(savedNotificationIntent);
+            UserHandle userHandle = mService.getSavedFrpNotificationUserHandle();
+            assertEquals(userHandle,
+                    UserHandle.of(mInjector.getUserManagerInternal().getMainUserId()));
 
-        String permission = mService.getSavedFrpNotificationPermission();
-        assertEquals(CONFIGURE_FACTORY_RESET_PROTECTION, permission);
+            String permission = mService.getSavedFrpNotificationPermission();
+            assertEquals(CONFIGURE_FACTORY_RESET_PROTECTION, permission);
+        } else {
+            assertNull(mService.getSavedFrpNotificationIntent());
+            assertNull(mService.getSavedFrpNotificationUserHandle());
+            assertNull(mService.getSavedFrpNotificationPermission());
+        }
     }
 
     private void checkPasswordHistoryLength(int userId, int expectedLen) {
