254e72e8d42d2dc11511e8ad0c2b65353dd69d2e
Michael Bestas <mkbestas@gmail.com>
2026-06-18 01:38:27 +0300

    fixup! Set FakeStore as Aurora Store installer package name
    Change-Id: I406a4a507cf59496f399469c7052b875fef26a08

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -5232,6 +5232,7 @@ public class ComputerEngine implements Computer {
                 || AURORA_SERVICES.equals(installerPackageName))) {
             return InstallSource.create(PLAY_STORE, PLAY_STORE, PLAY_STORE,
                             installSource.mInstallerPackageUid, // FIXME: likely wrong
+                            installSource.mInstallerPackageUid, // FIXME: likely wrong
                             installSource.mUpdateOwnerPackageName,
                             installSource.mInstallerAttributionTag,
                             PackageInstaller.PACKAGE_SOURCE_STORE)
